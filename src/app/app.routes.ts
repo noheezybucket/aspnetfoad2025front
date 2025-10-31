7b225e632eb19e5c5754fe5e058929085dbce92d
noheezybucket <seydinag023@gmail.com>
2025-10-31 01:18:15 +0000

    login and auth page front working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AuthComponent} from './auth/auth.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path:'login', component: LoginComponent },
+      { path:'register', component: RegisterComponent },
+    ]
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  }
+];
